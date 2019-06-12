99eed82abb70e47d9eec008cd66ed843df3f293a
Franco Mazzoni <franco.mazzoni@despegar.com>
2019-06-12 18:42:05 -0300

    Add chart all categories

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -32,6 +32,38 @@ class ReportsController < ApplicationController
                       @otras.select{|comment| comment.rating == 3}.count, @otras.select{|comment| comment.rating == 4}.count,
                       @otras.select{|comment| comment.rating == 5}.count]
 
+    @data_one = [@comments.select{|comment| comment.rating == 1}.select {|comment| comment.category == "Limpieza"}.count,
+                 @comments.select{|comment| comment.rating == 1}.select {|comment| comment.category == "Atención al cliente"}.count,
+                 @comments.select{|comment| comment.rating == 1}.select {|comment| comment.category == "Ubicación"}.count,
+                 @comments.select{|comment| comment.rating == 1}.select {|comment| comment.category == "Seguridad"}.count,
+                 @comments.select{|comment| comment.rating == 1}.select {|comment| comment.category == "Otras"}.count]
+
+    @data_two = [@comments.select{|comment| comment.rating == 2}.select {|comment| comment.category == "Limpieza"}.count,
+                 @comments.select{|comment| comment.rating == 2}.select {|comment| comment.category == "Atención al cliente"}.count,
+                 @comments.select{|comment| comment.rating == 2}.select {|comment| comment.category == "Ubicación"}.count,
+                 @comments.select{|comment| comment.rating == 2}.select {|comment| comment.category == "Seguridad"}.count,
+                 @comments.select{|comment| comment.rating == 2}.select {|comment| comment.category == "Otras"}.count]
+
+    @data_three = [@comments.select{|comment| comment.rating == 3}.select {|comment| comment.category == "Limpieza"}.count,
+                 @comments.select{|comment| comment.rating == 3}.select {|comment| comment.category == "Atención al cliente"}.count,
+                 @comments.select{|comment| comment.rating == 3}.select {|comment| comment.category == "Ubicación"}.count,
+                 @comments.select{|comment| comment.rating == 3}.select {|comment| comment.category == "Seguridad"}.count,
+                 @comments.select{|comment| comment.rating == 3}.select {|comment| comment.category == "Otras"}.count]
+
+    @data_four = [@comments.select{|comment| comment.rating == 4}.select {|comment| comment.category == "Limpieza"}.count,
+                 @comments.select{|comment| comment.rating == 4}.select {|comment| comment.category == "Atención al cliente"}.count,
+                 @comments.select{|comment| comment.rating == 4}.select {|comment| comment.category == "Ubicación"}.count,
+                 @comments.select{|comment| comment.rating == 4}.select {|comment| comment.category == "Seguridad"}.count,
+                 @comments.select{|comment| comment.rating == 4}.select {|comment| comment.category == "Otras"}.count]
+
+    @data_five = [@comments.select{|comment| comment.rating == 5}.select {|comment| comment.category == "Limpieza"}.count,
+                 @comments.select{|comment| comment.rating == 5}.select {|comment| comment.category == "Atención al cliente"}.count,
+                 @comments.select{|comment| comment.rating == 5}.select {|comment| comment.category == "Ubicación"}.count,
+                 @comments.select{|comment| comment.rating == 5}.select {|comment| comment.category == "Seguridad"}.count,
+                 @comments.select{|comment| comment.rating == 5}.select {|comment| comment.category == "Otras"}.count]
+
+
+
     @bar_chart_atencionalcliente = Gchart.bar(
         :type => 'bar',
         :size => '600x400',
@@ -106,5 +138,20 @@ class ReportsController < ApplicationController
         :axis_labels => ['1|2|3|4|5'],
         :stacked => false,
         )
+
+    @bar_chart_all = Gchart.bar(
+        :type => 'bar',
+        :size => '600x400',
+        :bar_colors => ['FFFF00', '0000FF', '00FF00', 'FF0000', '000000'],
+        :title => "Comentarios agrupados por categoría",
+        :bg => 'EFEFEF',
+        :legend => ['1 estrella', '2 estrellas', '3 estrellas', '4 estrellas', '5 estrellas'],
+        :data => [@data_one, @data_two, @data_three, @data_four, @data_five],
+        :axis_with_labels => [['x'], ['y']],
+        :axis_range => [nil, [0, @data_one.max]],
+        :min_value => 0,
+        :bar_width_and_spacing => '25,60',
+        :axis_labels => ['Limpieza|Atención al cliente|Ubicación|Seguridad|Otras'],
+    )
   end
 end
