6ff6d2aa62f56cab9be392250e0b2236cb31876f
maxisuppes <msuppes@eryx.co>
2019-06-30 19:33:00 -0300

    - Configuración de Rails Active Storage para administrar imagenes - Se puede, opcionalmente, tomar una foto con la cámara y adjuntarla en el comentario. El administrador ve esta foto en el detalle del comentario

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   validates :content, presence: true
   validates :rating, presence: true
   validates :category, presence: true
+
+  def comment_image
+    image.variant(resize: '400x300', rotate: '90', auto_orient: true)
+  end
 end
