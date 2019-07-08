6281c9675901ea2dde2daa272fb7caf93438b86f
ChrisFlorez <christian.florez@despegar.com>
2019-07-08 18:17:27 -0300

    se agrega la logica de filtrados para los comentarios(producto base)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -70,6 +70,20 @@ class CommentsController < ApplicationController
   def comment_error
   end
 
+  def filter
+    if params[:filter]
+      index
+      @selected_category = params[:category]
+      @start_date = params[:start_date_comment]
+      @end_date = params[:end_date_comment]
+      category = params[:category]
+      if category != "Todas"
+        @comments = filter_comments_category(category)
+      end
+      @comments = filter_comments_by_date
+    end
+  end
+
   private
   def send_notification_to(user_id, comment_data)
     @user = User.find(user_id)
@@ -78,5 +92,24 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content, :user_id, :name, :email, :rating, :category, :image)
+  end
+
+  def filter_comments_category(category)
+    @comments.where("category = :category", {category: category})
+  end
+
+  def filter_comments_by_date
+    start_date = params[:start_date_comment]
+    end_date = params[:end_date_comment]
+
+    if start_date.empty? && end_date.empty?
+      @comments
+    elsif start_date.empty?
+      @comments.where("created_at <= :end_date", {end_date: end_date})
+    elsif end_date.empty?
+      @comments.where("created_at >= :start_date", {start_date: start_date})
+    else
+      @comments.where(created_at: (start_date)..end_date)
+    end
   end
 end
