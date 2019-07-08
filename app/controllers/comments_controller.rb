64779f1345c0442de93ca24c6ea6882eae3d72ee
Franco Mazzoni <franco.mazzoni@despegar.com>
2019-07-08 16:41:44 -0300

    Add filter options in comments page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
+    @user = current_user
     @comments = current_user.comments.order('created_at desc')
     @user_url = url_for controller: 'comments', action: 'new', shop: current_user.hash_for_url
   end
