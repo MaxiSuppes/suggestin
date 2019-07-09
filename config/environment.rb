aceb44398a3f3603cbad87d75d1807e929f2741e
maxisuppes <msuppes@eryx.co>
2019-07-09 14:51:38 -0300

    Configuración de envío de emails con Sengrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Config mailer
-ActionMailer::Base.smtp_settings = {}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,11 +96,11 @@ Rails.application.configure do
 
   # Config email sender
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.default_url_options = { host: "https://reviewit-app.herokuapp.com" }
+  ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'yourdomain.com',
+      :domain => 'https://reviewit-app.herokuapp.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
